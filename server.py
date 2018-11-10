331f23b94c9d161a50fc7f2d54f98d01545b4e98
Vladimyr <chernvld@gmail.com>
2018-11-10 15:37:05 +0300

    Decode

--- server.py
+++ server.py
@@ -8,14 +8,14 @@ sock.bind(('', TCP_PORT))
 sock.listen(1)
 conn, addr = sock.accept()
 
-print ('connected:', addr)
+print('connected:', addr)
 
 while True:
     data = conn.recv(1024)
     if not data:
         break
 
-    print('user - ', addr, ' send - ', data.decode())
+    print('user - ', addr, ' send - ', str(data))
     conn.send(data.upper())
 
 conn.close()
