0a9404b0f45ff706737f7b8dddf6c8ad0a88e9db
Vladimyr <chernvld@gmail.com>
2018-11-10 15:47:00 +0300

    Decoding msg

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ while True:
     if not data:
         break
 
-    print('user - ', addr, ' send - ', str(data))
+    print('user - ', addr, ' send - ', data)
     conn.send(data.upper())
 
 conn.close()
