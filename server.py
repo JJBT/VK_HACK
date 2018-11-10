03ebd600bcdcd8e5fab7e13da0b863dc530a39e9
Vladimyr <chernvld@gmail.com>
2018-11-10 15:26:09 +0300

    Second commit

--- server.py
+++ server.py
@@ -8,12 +8,14 @@ sock.bind(('', TCP_PORT))
 sock.listen(1)
 conn, addr = sock.accept()
 
-print('connected:', addr)
+print ('connected:', addr)
 
 while True:
     data = conn.recv(1024)
     if not data:
         break
+
+    print('user - ', addr, ' send - ', data)
     conn.send(data.upper())
 
 conn.close()
